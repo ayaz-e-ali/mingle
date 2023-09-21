05d77c664cb7734dac7373fa3f4fb2496d4bb7f2
Mhd-alali <hsinsa758@gmail.com>
2023-09-21 11:54:48 +0300

    changed file upload to show the uploaded images

--- components/forms/Fileupload.jsx
+++ components/forms/Fileupload.jsx
@@ -1,22 +1,29 @@
 /* eslint-disable react/prop-types */
+import Image from 'next/image';
 import Dropzone from "react-dropzone";
 
-export default function FileUpload({ onDrop, file }) {
-    
-
-    return <Dropzone acceptedFiles={'.jpg,.jpeg,.png'} multiple={false} onDrop={onDrop} >
+export default function FileUpload({ onDrop, file, multiple = false }) {
+    const exist = Array.isArray(file) && file.length > 0 || file.size;
+    return <Dropzone acceptedFiles={'.jpg,.jpeg,.png'} multiple={multiple} onDrop={onDrop} >
         {({ getRootProps, getInputProps }) => (
-            <div className='col-span-2 p-4 rounded-md border mt-4' >
+            <div className='p-4 rounded-md border mt-4' >
                 <div className={`cursor-pointer rounded pl-3 border-dashed border-2 border-accent`} {...getRootProps()}>
                     <input {...getInputProps()} />
-                    {!file.size
-                        ? <p className='my-4'>Add picture here</p>
-                        : <div className="flexbetween my-4 mr-4">
-                            <p>{file.name}</p>
+                    {exist
+                        ? <div className="flex flex-wrap gap-1 my-4 mr-4">
+                            {
+                                Array.isArray(file) ?
+                                    file.map((img) => (
+                                        <Image key={img.name} src={URL.createObjectURL(img)} alt="uploaded" width={60} height={60} className='object-cover rounded-md'/>
+                                    ))
+                                    :
+                                    <Image src={URL.createObjectURL(file)} alt="uploaded" width={60} height={60} className='object-cover rounded-md'/>
+                            }
                         </div>
+                        : <p className='my-4'>Add picture here</p>
                     }
                 </div>
             </div>
         )}
-    </Dropzone>
+    </Dropzone>;
 }
