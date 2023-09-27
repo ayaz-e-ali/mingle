17b39c296dc73f0caf274e056c45b1d030c6831c
Mhd-alali <hsinsa758@gmail.com>
2023-09-27 14:12:24 +0300

    fixed unique key problem in bottom nav

--- components/nav/BottomNav.jsx
+++ components/nav/BottomNav.jsx
@@ -11,16 +11,14 @@ export default function BottomNav() {
         <div className="flex justify-evenly gap-6 fixed lg:hidden w-full backdrop-blur-lg bg-secondary/20 py-4 bottom-0 ">
             {
                 NavbarLinks.map(item => (
-                    <>
-                        <Link href={item.route} key={item.label} className={cn('p-2 rounded-md flex flex-col items-center gap-1', pathname === item.route && 'bg-accent')}>
-                            <span >
-                                {item.Icon}
-                            </span>
-                            <span className='text-xs'>
-                                {item.label}
-                            </span>
-                        </Link>
-                    </>
+                    <Link href={item.route} key={item.label} className={cn('p-2 rounded-md flex flex-col items-center gap-1', pathname === item.route && 'bg-accent')}>
+                        <span >
+                            {item.Icon}
+                        </span>
+                        <span className='text-xs'>
+                            {item.label}
+                        </span>
+                    </Link>
                 ))
             }
         </div>
